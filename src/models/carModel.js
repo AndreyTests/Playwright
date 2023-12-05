f45f1c8cd6f7f37fb2e0767b2d4fc4acb189c7df
Andriy <andriimatrix@gmail.com>
2023-12-05 22:59:48 +0200

    Revert "Home work API Controllers"
    This reverts commit f63a8c071bce1587a2398bf3e921db61a5fc8953.

--- src/models/carModel.js
+++
@@ -1,8 +0,0 @@
-
-export default class CarModel {
-    constructor({carBrandId, carModelId, mileage}) {
-        this.carBrandId = carBrandId
-        this.carModelId = carModelId
-        this.mileage = mileage
-    }
-}
